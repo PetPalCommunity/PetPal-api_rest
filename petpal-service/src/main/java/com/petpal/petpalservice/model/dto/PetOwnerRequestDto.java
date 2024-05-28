6f1c9eef0dc4a8da597c5aa3b74129df3cb498a1
SamuelCano03 <U202116508@upc.edu.pe>
2024-05-28 14:14:29 -0500

    feature: visibility

--- petpal-service/src/main/java/com/petpal/petpalservice/model/dto/PetOwnerRequestDto.java
+++ petpal-service/src/main/java/com/petpal/petpalservice/model/dto/PetOwnerRequestDto.java
@@ -9,10 +9,46 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PetOwnerRequestDto {
-    private  String ownerName;
-    private int ownerAge;
-    private String ownerSex;
-    private int ownerPhone;
-    private String ownerEmail;
-    private String ownerPassword;
+  private  String ownerName;
+  private int ownerAge;
+  private String ownerSex;
+  private int ownerPhone;
+  private String ownerEmail;
+  private String ownerPassword;
+  public String getOwnerName() {
+    return ownerName;
+  }
+  public void setOwnerName(String ownerName) {
+    this.ownerName = ownerName;
+  }
+  public int getOwnerAge() {
+    return ownerAge;
+  }
+  public void setOwnerAge(int ownerAge) {
+    this.ownerAge = ownerAge;
+  }
+  public String getOwnerSex() {
+    return ownerSex;
+  }
+  public void setOwnerSex(String ownerSex) {
+    this.ownerSex = ownerSex;
+  }
+  public int getOwnerPhone() {
+    return ownerPhone;
+  }
+  public void setOwnerPhone(int ownerPhone) {
+    this.ownerPhone = ownerPhone;
+  }
+  public String getOwnerEmail() {
+    return ownerEmail;
+  }
+  public void setOwnerEmail(String ownerEmail) {
+    this.ownerEmail = ownerEmail;
+  }
+  public String getOwnerPassword() {
+    return ownerPassword;
+  }
+  public void setOwnerPassword(String ownerPassword) {
+    this.ownerPassword = ownerPassword;
+  }
 }
--- petpal-service/src/main/java/com/petpal/petpalservice/model/dto/VetRequestDto.java
+++ petpal-service/src/main/java/com/petpal/petpalservice/model/dto/VetRequestDto.java
@@ -8,12 +8,60 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VetRequestDto {
-    private String vetName;
-    private String vetLastname;
-    private String vetSex;
-    private int vetPhone;
-    private String vetLocation;
-    private String vetEmail;
-    private String vetPassword;
-    private int vetLicenseNumber;
+  private String vetName;
+  private String vetLastname;
+  private String vetSex;
+  private int vetPhone;
+  private String vetLocation;
+  private String vetEmail;
+  private String vetPassword;
+  private int vetLicenseNumber;
+  public String getVetName() {
+    return vetName;
+  }
+  public void setVetName(String vetName) {
+    this.vetName = vetName;
+  }
+  public String getVetLastname() {
+    return vetLastname;
+  }
+  public void setVetLastname(String vetLastname) {
+    this.vetLastname = vetLastname;
+  }
+  public String getVetSex() {
+    return vetSex;
+  }
+  public void setVetSex(String vetSex) {
+    this.vetSex = vetSex;
+  }
+  public int getVetPhone() {
+    return vetPhone;
+  }
+  public void setVetPhone(int vetPhone) {
+    this.vetPhone = vetPhone;
+  }
+  public String getVetLocation() {
+    return vetLocation;
+  }
+  public void setVetLocation(String vetLocation) {
+    this.vetLocation = vetLocation;
+  }
+  public String getVetEmail() {
+    return vetEmail;
+  }
+  public void setVetEmail(String vetEmail) {
+    this.vetEmail = vetEmail;
+  }
+  public String getVetPassword() {
+    return vetPassword;
+  }
+  public void setVetPassword(String vetPassword) {
+    this.vetPassword = vetPassword;
+  }
+  public int getVetLicenseNumber() {
+    return vetLicenseNumber;
+  }
+  public void setVetLicenseNumber(int vetLicenseNumber) {
+    this.vetLicenseNumber = vetLicenseNumber;
+  }
 }
