ce52c9527e92ef842e7fd2029a23ef56111c8fcb
Eduardo Puglisevich <eduardopugli2508@gmail.com>
2024-05-30 16:54:09 -0500

    Unit and Integration tests pets

--- petpal-service/src/main/java/com/petpal/petpalservice/repository/ReminderRepository.java
+++ petpal-service/src/main/java/com/petpal/petpalservice/repository/ReminderRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.petpal.petpalservice.model.entity.Pet;
 import com.petpal.petpalservice.model.entity.Reminder;
 
 public interface ReminderRepository extends JpaRepository<Reminder, Integer>{
